5dcfa764ddfbb8ed051cafb94a8194262572929e
jnj574jj7m-source <jnj574jj7m@privaterelay.appleid.com>
2026-09-21 14:43:33 +0530

    Update ADMIN_ID to new Telegram User ID

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ warnings.filterwarnings('ignore')
 # ================================================================
 # Render Environment Variable aayi BOT_TOKEN edukkunnu
 BOT_TOKEN = os.environ["BOT_TOKEN"]  
-ADMIN_ID = 6876395516                        # Replace with your Telegram User ID
+ADMIN_ID = 7212602902                      # Replace with your Telegram User I
 
 API_KEY = 'AIzaSyCQDz9rgjgmvmFkvVfmvr2-7fT4tfrzRRQ'
 CF_BASE = 'https://europe-west1-cpm-2-7cea1.cloudfunctions.net'
